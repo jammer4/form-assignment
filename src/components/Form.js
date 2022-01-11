501d54011e5cbcc07d3563b20ab26b9b18f7d019
jammer4 <jarrettammerman@gmail.com>
2022-01-11 15:50:38 -0500

    Checkbox works

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,16 @@
+import { useState } from 'react';
+
 function Form() {
-    function enableHypo() {
-        const hypo = document.getElementById('hypo-check');
-        const hypoText = document.getElementById('hypo-mean');
-        const hypoLabel = document.getElementById('hypo-label');
-        hypoText.disabled = hypo.checked ? false : true;
-        if (hypoText.disabled) {
-            hypoLabel.setAttribute('style', 'color: darkslategrey');
+    const [disable, setDisable] = useState(true);
+    function checkClick() {
+        setDisable(!disable);
+
+        const label = document.getElementById('hypo-label');
+        if (disable) {
+            label.style.color = 'darkslategray';
         }
         else {
-            hypoLabel.setAttribute('style', 'color: lightgray');
+            label.style.color = 'lightgray';
         }
     }
     
@@ -21,10 +23,10 @@ function Form() {
                 <input type="text" id="sample-mean"></input><br/>
                 <label htmlFor="standard-dev">Standard deviation: </label>
                 <input type="text" id="standard-dev"></input><br/>
-                <input type="checkbox" id="hypo-check" onClick={enableHypo}></input>
+                <input type="checkbox" id="hypo-check" onClick={checkClick}></input>
                 <label htmlFor="hypo-check">Perform hypothesis test</label><br/><br/>
-                <label htmlFor="hypo-mean" id="hypo-label" style={{color: "lightgray"}}>Hypothesized mean: </label>
-                <input type="text" id="hypo-mean" disabled></input><br/>
+                <label htmlFor="hypo-mean" id="hypo-label" style={{color: 'lightgray'}}>Hypothesized mean: </label>
+                <input type="text" id="hypo-mean" disabled={disable}></input><br/>
                 <button id="ok">OK</button>
                 <button id ="reset">Reset</button>
             </form>
