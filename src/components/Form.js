3fd2d98844fde32b066fbee75b37f10b605b8a20
jammer4 <jarrettammerman@gmail.com>
2022-01-11 14:25:40 -0500

    end of day 1

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,17 @@
 function Form() {
+    function enableHypo() {
+        const hypo = document.getElementById('hypo-check');
+        const hypoText = document.getElementById('hypo-mean');
+        const hypoLabel = document.getElementById('hypo-label');
+        hypoText.disabled = hypo.checked ? false : true;
+        if (hypoText.disabled) {
+            hypoLabel.setAttribute('style', 'color: darkslategrey');
+        }
+        else {
+            hypoLabel.setAttribute('style', 'color: lightgray');
+        }
+    }
+    
     return (
         <div id="form-div">
             <form id="main-form">
@@ -8,10 +21,10 @@ function Form() {
                 <input type="text" id="sample-mean"></input><br/>
                 <label htmlFor="standard-dev">Standard deviation: </label>
                 <input type="text" id="standard-dev"></input><br/>
-                <input type="checkbox" id="hypo-check"></input>
+                <input type="checkbox" id="hypo-check" onClick={enableHypo}></input>
                 <label htmlFor="hypo-check">Perform hypothesis test</label><br/><br/>
-                <label htmlFor="hypo-mean" id="hypo-label">Hypothesized mean: </label>
-                <input type="text" id="hypo-mean"></input><br/>
+                <label htmlFor="hypo-mean" id="hypo-label" style={{color: "lightgray"}}>Hypothesized mean: </label>
+                <input type="text" id="hypo-mean" disabled></input><br/>
                 <button id="ok">OK</button>
                 <button id ="reset">Reset</button>
             </form>
