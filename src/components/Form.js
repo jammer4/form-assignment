6f221808522b5f9bb8364cd4d83b86379acad1b8
jammer4 <jarrettammerman@gmail.com>
2022-01-12 07:42:03 -0500

    finished validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,59 +16,57 @@ function Form() {
     }
 
     function validate() {
-        const size = document.getElementById('sample-size').value;
-        if (size === '') {
+        const size = document.getElementById('sample-size');
+        if (size.value === '') {
             alert('Please fill in all active fields');
             return;
         }
-        if (isNaN(size)) {
+        if (isNaN(size.value)) {
             alert('Only numbers are allowed for this field');
             return;
         }
-        if (parseInt(size) < 2) {
-            alert('Only whole numbers greater than 2 are allowed');
+        if (parseInt(size.value) < 2) {
+            alert('Only whole numbers greater than 1 are allowed');
             return;
         }
 
-        const mean = document.getElementById('sample-mean').value;
-        if (mean === '') {
+        const mean = document.getElementById('sample-mean');
+        if (mean.value === '') {
             alert('Please fill in all active fields');
             return;
         }
-        if (isNaN(mean)) {
+        if (isNaN(mean.value)) {
             alert('Only numbers are allowed for this field');
             return;
         }     
        
-        const dev = document.getElementById('standard-dev').value;
-        if (dev === '') {
+        const dev = document.getElementById('standard-dev');
+        if (dev.value === '') {
             alert('Please fill in all active fields');
             return;
         }
-        if (isNaN(dev)) {
+        if (isNaN(dev.value)) {
             alert('Only numbers are allowed for this field');
             return;
         }
-        if (parseInt(dev) <= 0) {
+        if (parseInt(dev.value) <= 0) {
             alert('Only numbers greater than 0 are allowed');
             return;
         }
 
-        const hypo = document.getElementById('hypo-mean').value;
-        if (hypo === '') {
-            alert('Please fill in all active fields');
-            return;
-        }
-        if (isNaN(hypo)) {
-            alert('Only numbers are allowed for this field');
-            return;
+        const hypo = document.getElementById('hypo-mean');
+        if (!hypo.disabled) {
+            if (hypo.value === '') {
+                alert('Please fill in all active fields');
+                return;
+            }
+            if (isNaN(hypo.value)) {
+                alert('Only numbers are allowed for this field');
+                return;
+            }
         }
     }
 
-    function reset() {
-        document.getElementById('sample-size').value = '';
-    }
-    
     return (
         <div id="form-div">
             <form id="main-form">
@@ -82,11 +80,11 @@ function Form() {
                 <label htmlFor="hypo-check">Perform hypothesis test</label><br/><br/>
                 <label htmlFor="hypo-mean" id="hypo-label" style={{color: 'lightgray'}}>Hypothesized mean: </label>
                 <input type="text" id="hypo-mean" disabled={disable}></input><br/>
-                <button id="ok" onClick={validate}>OK</button>
-                <button id ="reset">Reset</button>
+                <button id="ok" type="button" onClick={validate}>OK</button>
+                <button id ="reset" type="reset">Reset</button>
             </form>
         </div>
     );
 }
 
-export default Form;
+export default Form; 
