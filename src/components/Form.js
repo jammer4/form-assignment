bc75c85da2e45558f6ad3f53e1665b0acf649778
jammer4 <jarrettammerman@gmail.com>
2022-01-11 21:14:55 -0500

    form validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 function Form() {
     const [disable, setDisable] = useState(true);
+    
     function checkClick() {
         setDisable(!disable);
 
@@ -12,6 +13,60 @@ function Form() {
         else {
             label.style.color = 'lightgray';
         }
+    }
+
+    function validate() {
+        const size = document.getElementById('sample-size').value;
+        if (size === '') {
+            alert('Please fill in all active fields');
+            return;
+        }
+        if (isNaN(size)) {
+            alert('Only numbers are allowed for this field');
+            return;
+        }
+        if (parseInt(size) < 2) {
+            alert('Only whole numbers greater than 2 are allowed');
+            return;
+        }
+
+        const mean = document.getElementById('sample-mean').value;
+        if (mean === '') {
+            alert('Please fill in all active fields');
+            return;
+        }
+        if (isNaN(mean)) {
+            alert('Only numbers are allowed for this field');
+            return;
+        }     
+       
+        const dev = document.getElementById('standard-dev').value;
+        if (dev === '') {
+            alert('Please fill in all active fields');
+            return;
+        }
+        if (isNaN(dev)) {
+            alert('Only numbers are allowed for this field');
+            return;
+        }
+        if (parseInt(dev) <= 0) {
+            alert('Only numbers greater than 0 are allowed');
+            return;
+        }
+
+        const hypo = document.getElementById('hypo-mean').value;
+        if (hypo === '') {
+            alert('Please fill in all active fields');
+            return;
+        }
+        if (isNaN(hypo)) {
+            alert('Only numbers are allowed for this field');
+            return;
+        }
+    }
+
+    function reset() {
+        document.getElementById('sample-size').value = '';
     }
     
     return (
@@ -27,7 +82,7 @@ function Form() {
                 <label htmlFor="hypo-check">Perform hypothesis test</label><br/><br/>
                 <label htmlFor="hypo-mean" id="hypo-label" style={{color: 'lightgray'}}>Hypothesized mean: </label>
                 <input type="text" id="hypo-mean" disabled={disable}></input><br/>
-                <button id="ok">OK</button>
+                <button id="ok" onClick={validate}>OK</button>
                 <button id ="reset">Reset</button>
             </form>
         </div>
